3110137c9b5e32e97fecda9f32dbb0f38db482fd
infotekgat <t.gasparic@infotek-software.com>
2016-12-12 15:48:06 +0100

    test

--- WSServer.js
+++ WSServer.js
@@ -68,8 +68,10 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(conf.port, function () {
-    var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function () {
+   
     LogFuction(messageType.info, ((new Date()) + ' Server is listening on port: ' + server_port));
 });
  
